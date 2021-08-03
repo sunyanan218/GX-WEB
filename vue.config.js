f4b14718493d4f94ea2ecfeb1de6a0ec3229aa4b
sunyanan <sunyanan218@gmail.com>
2021-08-03 22:42:02 +0800

    修改页面 title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  lintOnSave: false,
+  // lintOnSave: false,
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "高仙机器人任务管理平台";
+      return args;
+    });
+  },
 };
